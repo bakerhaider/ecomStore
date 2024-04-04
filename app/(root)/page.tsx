f3d2cd7494b1fc211555f8ebc1dc245f68f5f707
BakerAlblam <gallardo0@outlook.com>
2024-04-04 14:36:56 +0200

    props

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,23 @@
-import { UserButton } from '@clerk/nextjs';
+import {
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
 import React from 'react';
 
 const Home = () => {
-  return <div>jej</div>;
+  return (
+    <div className="text-white">
+      <SignedIn>
+        <SignOutButton>out</SignOutButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton>in</SignInButton>
+      </SignedOut>
+    </div>
+  );
 };
 
 export default Home;
