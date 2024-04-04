d28672aebb5599eff5cea9981e90acb97b8921be
BakerAlblam <gallardo0@outlook.com>
2024-04-04 14:25:14 +0200

    cleaner

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,17 +1,7 @@
-import MobileNav from '@/components/shared/MobileNav';
-import Sidebar from '@/components/shared/Sidebar';
 import React from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <main className="root">
-      {/*  <Sidebar />
-      <MobileNav /> */}
-      <div className="root-container">
-        <div className="wrapper"> {children} </div>
-      </div>
-    </main>
-  );
+  return <div className="">{children}</div>;
 };
 
 export default Layout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { IBM_Plex_Sans } from 'next/font/google';
 import './globals.css';
-import { cn } from '@/lib/utils';
+
 import { ClerkProvider } from '@clerk/nextjs';
 
 const IBMPlex = IBM_Plex_Sans({
@@ -27,9 +27,7 @@ export default function RootLayout({
       }}
     >
       <html lang="en">
-        <body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>
-          {children}
-        </body>
+        <body>{children}</body>
       </html>
     </ClerkProvider>
   );
